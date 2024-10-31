ca232c2374c1b2bf42981cc3b82922e4bce84f28
Leana Le <180620306+chuot-the-rat@users.noreply.github.com>
2024-10-31 14:34:02 -0700

    Update problem.js

--- problem.js
+++ problem.js
@@ -13,3 +13,50 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+/* 
+making the function take in the password and user input
+first checking if the password is correct or something else in entered like reset or forgot 
+then if none of the options are entered (correct pass, forgot, or reset) then they will be denied access 
+that is why it is the last else statement 
+
+i first checked forgot and reset because if its true AND is one of those special cases it would log out a unique message 
+then if its just true it can log out access granted
+*/
+
+function passwordCheck(password, userInput) {
+    if (userInput === "forgot" && password === "forgot"){
+        console.log("Access Granted!" + " This password you set should not be used because it glitches the system")
+    } else if (userInput === "forgot"){    
+        console.log("Here is a hint")
+    } else if (userInput === "reset" && password === "reset"){
+        console.log("Access Granted!" + " This password you set should not be used because it glitches the system")
+    } else if (userInput === "reset"){
+        console.log("Let's reset your account")
+    } else if (password === userInput && password.length >= 5){
+        console.log("Access Granted!")
+    } else if (password === userInput && password.length < 5){
+        console.log("Your password is too short!")
+    } else {
+        console.log("Access Denied!")
+    }
+}
+
+
+passwordCheck("passw0rd","forgot")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
